8755fc5a06125158c33bbd67602904432c308ba2
James Raphael Vibar <vibarjamess@gmail.com>
2018-11-24 20:45:18 +0800

    Updates

---
+++ precache-manifest.f77e02b5074933b2fdd304b88cbaa493.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "d45e9bf326909af988bf",
+    "url": "/contactmanager/static/js/main.d45e9bf3.chunk.js"
+  },
+  {
+    "revision": "aa01e843918f5c269004",
+    "url": "/contactmanager/static/js/1.aa01e843.chunk.js"
+  },
+  {
+    "revision": "d45e9bf326909af988bf",
+    "url": "/contactmanager/static/css/main.63a54989.chunk.css"
+  },
+  {
+    "revision": "aa01e843918f5c269004",
+    "url": "/contactmanager/static/css/1.60c6b10e.chunk.css"
+  },
+  {
+    "revision": "1fc692bd427cbd39aaa132c9bba62996",
+    "url": "/contactmanager/index.html"
+  }
+];
